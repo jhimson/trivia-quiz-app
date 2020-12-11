a461f9736c737db07f522762e1c3c6cb1c7fdc37
jhimson <jhimsonrayraboypamisa@gmail.com>
2020-12-11 06:20:16 -0500

    Fixed eslint errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-4xl text-blue-500'>Hello, world!</h1>
+      <h1 className="text-4xl text-blue-500">Hello, world!</h1>
     </div>
   );
 }
