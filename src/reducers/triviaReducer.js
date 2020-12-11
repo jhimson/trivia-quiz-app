19c606791ceda57a786f825aca619d1f9e8a5b72
jhimson <jhimsonrayraboypamisa@gmail.com>
2020-12-11 05:54:11 -0500

    Added triviaReducer

---
+++ src/reducers/triviaReducer.js
@@ -0,0 +1,31 @@
+import triviaConstants from '../constants'
+
+const {FETCH_TRIVIA_REQUEST, FETCH_TRIVIA_SUCCESS, FETCH_TRIVIA_FAIL} = triviaConstants;
+
+export const triviaReducer = (state = {}, {type, payload}) => {
+    switch(type){
+
+        case FETCH_TRIVIA_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+
+        case FETCH_TRIVIA_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                triviaList: payload
+            }
+        
+        case FETCH_TRIVIA_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: payload
+            }
+
+        default:
+            return state;
+    }
+}
